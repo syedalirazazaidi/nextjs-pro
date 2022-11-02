7c2b3fdd15613242daeb8fa1733231e8692bd754
DESKTOP-7V9Q99B\AKU <akuraza6atgmail.com>
2022-11-02 11:13:56 +0500

    add client server

--- app/shared/wordgenerator.tsx
+++ app/shared/wordgenerator.tsx
@@ -1,12 +1,20 @@
 "use client"
-import { useEffect } from "react"
-
+import { useEffect, useState } from "react"
+const words =['hello','hi','network','developer']
 
 export default function WordGenerator() {
+    const [word ,setWord] = useState('Not yet defined :(')
+    const generate=()=>{
+        const randomWord = words[Math.floor(Math.random()*words.length)]
+        setWord(randomWord)
+    }
     useEffect(()=>{
         console.log('client component')
     })
   return (
-    <div>wordgenerator</div>
+    <div><button onClick={generate}>
+        wordgenerator</button>
+        <p> {word}</p>
+        </div>
   )
 }
